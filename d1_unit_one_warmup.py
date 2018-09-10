ac0420ca1919557b8089eeb6eb425a858ee89880
Jonathan <jonathan@iMac9.local>
2018-09-10 08:32:37 -0400

    Unit one warm up, day one

---
+++ d1_unit_one_warmup.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+turtle.forward(100)
+
+for x in range (3):
+    turtle.left(120)
+    turtle.forward(100)
+
+turtle.exitonclick()
