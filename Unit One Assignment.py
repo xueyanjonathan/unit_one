757ae0cecb125ed207e83503e48b51042cad2728
Jonathan <jonathan@iMac9.local>
2018-09-11 11:08:21 -0400

    Color in Circle

---
+++ Unit One Assignment.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.speed(50)
+turtle.fillcolor("pink")
+turtle.begin_fill()
+turtle.circle(200)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+
+turtle.left(90)
+for x in range(3):
+   
+
+
+turtle.exitonclick()
